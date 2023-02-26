f771c4af6c73ce79788668bf3d2cea0218c8e134
den4ik-chiller <denisp317@yandex.ru>
2023-02-26 13:03:42 +0300

    final_animationLine

---
+++ AnimationLine/java/com/example/animationline/HelloController.java
@@ -0,0 +1,14 @@
+package java_stepik.AnimationLine.java.com.example.animationline;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
