6691557e3a6fb241d66e1cd635f9b78114420035
den4ik-chiller <denisp317@yandex.ru>
2023-02-26 17:23:50 +0300

    final_RegistrationForm

---
+++ RegistrationForm/Main.java
@@ -0,0 +1,14 @@
+package java_stepik.RegistrationForm;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();        // Создаем объект класса MainWindow
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//способ выхода из формы
+        mainWindow.setLocationByPlatform(true);//устанавливаем местоположение
+        mainWindow.setTitle("Регистрация на мероприятие");//заголовок формы
+        mainWindow.pack();//упаковываем все на форме
+        mainWindow.setVisible(true);// Отображаем созданное окно
+    }
+}
