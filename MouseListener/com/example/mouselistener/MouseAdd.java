71e38280d82e71db087b2459d409a713e4e3f578
den4ik-chiller <denisp317@yandex.ru>
2023-02-26 14:57:07 +0300

    final_MouseListener

---
+++ MouseListener/com/example/mouselistener/MouseAdd.java
@@ -0,0 +1,40 @@
+package java_stepik.MouseListener.com.example.mouselistener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+public class MouseAdd {
+    static JFrame frame = new JFrame();
+        static JLayeredPane panel = new JLayeredPane();
+
+        static public void add(MouseEvent e){
+            if (e.getButton()==1){
+                JLabel label = new JLabel("X:"+e.getX()+" Y:"+e.getY());
+                label.setBounds(e.getX(),e.getY(),100,20);
+                panel.add(label);
+            }
+        }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setTitle("Добавление мышкой");
+                Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+                int width = 400, height = 400;
+                frame.setBounds(dim.width / 2 - width / 2, dim.height / 2 - height / 2, width, height);
+                panel.setFocusable(true);
+                frame.add(panel);
+                panel.addMouseListener(new MouseAdapter() {
+                    public void mouseClicked(MouseEvent e) {
+                        add(e);
+                    }
+                });
+                frame.setVisible(true);
+            }
+        });
+    }
+}
